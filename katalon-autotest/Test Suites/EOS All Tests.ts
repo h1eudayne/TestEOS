984d21d6d8146017ae3fc376f9da16cfa675a08f
Little Boy's h1eudayne <voduchieu42@gmail.com>
2026-03-17 04:50:49 +0700

    Add Katalon self-hosted runner workflow and test suite

---
+++ katalon-autotest/Test Suites/EOS All Tests.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Run all EOS Client tests</description>
+   <name>EOS All Tests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a1b2c3d4-e5f6-7890-abcd-ef1234567890</testSuiteGuid>
+   <testCaseLink>
+      <guid>f1a9279d-d683-454e-a22c-0f000bad0b13</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EOS Login Test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
